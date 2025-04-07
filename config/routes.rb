2e3d5193d8c91a3f0ee51b6eca16386c1664aadb
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-07 16:29:49 -0700

    Re-add home/index route to fix home controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   root "home#index"
+  get "home/index"
   get "home/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
